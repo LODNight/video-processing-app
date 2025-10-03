2e2753fe10bc11ee1fc9d73ca2c42af47a045b65
LODNight <khuunhattin123@gmail.com>
2025-10-03 19:51:33 +0700

    add debug

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file, flash
+from werkzeug.utils import secure_filename
 import ffmpeg
+import os
+import json
+from datetime import datetime
 
-# Chuyển đổi video
-(
-    ffmpeg.input()
-)
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    # return "Hello"
+    return render_template('index.html')
+
+@app.route('/user/<username>')
+def show_user(username):
+    return f"User: {username}"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
